efbe14425fa11c4b7c6fdc367d9a5a865018f9fb
Thomas Nommensen <thomas.nommensen@coepp.org.au>
2017-07-27 18:00:51 +1000

    Almost complete python. Need to add the future arguments sections and implement it to MadShell

--- DelphesReader.py
+++ DelphesReader.py
@@ -1,4 +1,5 @@
 import ROOT
+import sys
 #============Functions================#
 #================ Extracting Data from Delphes root =====================#
 def RootReading ( directory, TreeName , Branch, LeafName ):
@@ -28,21 +29,46 @@ def TreeLeaves ( directory, TreeName, LeafName):
 		LeafValues.append(Leaf)
 	return LeafValues
 #===========End Delphes Tree =====================#
+
+############Program Start##########################
+
+#============Future Input agruments===============#
+#directory1 = str(sys.argv[1]) 
 directory1= str("MadGraph5/bin/pp/Events/run_01/tag_1_delphes_events.root")
-x = RootReading(directory1, "Delphes", "MissingET", "MissingET.MET")
-y = TreeLeaves(directory1, "Delphes", "Jet_size")
+TreeNames = "Delphes"
+BranchName = "MissingET"
+LeafNamey = "Jet_size"
+LeafNamex = "MissingET.MET"
+PlotName = "Hello"
+Cutx = 0
+Cuty = 0
+#===========End Future Input arguments============#
+
+x = RootReading(directory1, TreeNames, BranchName, LeafNamex)
+y = TreeLeaves(directory1, TreeNames, LeafNamey)
+
 #===== Finding the max and min=======#
 max_x = int(max(x))
 max_y = int(max(y))
 min_x = int(min(x))
 min_y = int(min(y))
 #====================================#
+
+#========== ROOT Figures ============#
 binsy = int(max_y-min_y)
 binsx = int(max_x-min_x)
-h2 = ROOT.TH2F("h2", "hello", binsx, min_x , max_x, binsy, min_y, max_y)
+
+h2 = ROOT.TH2F("Figure", PlotName, binsx, min_x , max_x, binsy, min_y, max_y)
 length = len(y)
 for i in range(length):
 	valuey = y[i]
-	valuex = x[i]	
-	h2.Fill(valuex, valuey)
-h2.Draw("lego")
+	valuex = x[i]
+	if (Cuty <= valuey) and (Cutx <=valuex):
+		Collecty = valuey 
+		Collectx = valuex
+		h2.Fill(Collectx, Collecty)
+h2.GetXaxis().SetTitle(LeafNamex)
+h2.GetYaxis().SetTitle(LeafNamey)
+h2.GetZaxis().SetTitle("Frequency")
+h2.SetStats(1)
+h2.Draw("lego20")
