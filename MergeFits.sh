3d511427917cf099b2be37f174b27d9e904da48e
Thomas Nommensen <thomas.nommensen@coepp.org.au>
2017-08-27 21:32:38 +1000

    improved

--- MergeFits.sh
+++ MergeFits.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 #!/bin/bash
 
-directory=~/Plots/*
-cd ~/Plots/
-jets0after=()
-jetsafter=()
-jets0before=()
-jetsbefore=()
-for file in $directory
+directory=~/Plots #<------------------- change to desired
+first=$directory/*
+cd $directory
+for file in $first
 do
 	echo "$file"
 	name=${file##*/}
@@ -20,30 +17,6 @@ do
 	label="$(cut -d "_" -f2 <<< $actual)"
 	label2="$(cut -d "_" -f4 <<< $actual)"
 	label3="$(sed "s/[^0-9]//g" <<< $label2)"
-	if [[ "$label" == "jets0" ]];
-	then
-		if [[ "$label2" == "before"* ]];
-		then 
-			name="can_""$label""_met_""beforeFit$label3"".jpg"
-			jets0before+=("$name")
-		elif [[ "$label2" == "after"* ]];
-		then
-			name="can_""$label""_met_""afterFit$label3"".jpg"
-			jets0after+=("$name")
-		fi
-	fi
-	if [[ "$label" == "jets" ]];
-	then 
-		if [[ "$label2" == "before"* ]];
-		then 
-			name="can_""$label""_met_""$label2"".jpg"
-			jetsbefore+=("$name")
-		elif [[ "$label2" == "after"* ]];
-		then		
-			name="can_""$label""_met_""$label2"".jpg"
-			jetsafter+=("$name")
-		fi
-	fi
 	comment="$label$label2"
 	rm $file
 	rm $newfile
@@ -51,44 +24,45 @@ do
 	rm $output
 done
 
-for i in "${!jets0after[@]}";
+Jets0before=$directory/can_jets0_met_beforeFit*
+Jets0after=$directory/can_jets0_met_afterFit*
+for i in $Jets0after
 do
-	
-	nameJet0a="${jets0after[$i]}"
-	label2a="$(cut -d "_" -f4 <<< $nameJet0a)"
-	label3a="$(sed "s/[^0-9]//g" <<< $label2a)"
-	for z in "${!jets0after[@]}";
-	do 
-		nameJet0b="${jets0before[$z]}"
-		label2b="$(cut -d "_" -f4 <<< $nameJet0b)"
-		label3b="$(sed "s/[^0-9]//g" <<< $label2b)"
-		if [[ "$label3b" == "$label3a" ]];
-		then 
-			inputa="can_jets0_met_afterFit$label3a.jpg"
-			inputb="can_jets0_met_beforeFit$label3b.jpg"
-			output="can_jets0_met_$label3a.jpg"
-			convert +append $inputa $inputb $output
+	string="${i##*/}"
+	tempstring="$(cut -d "_" -f4 <<< $string)"
+	events="$(sed "s/[^0-9]//g" <<< $tempstring)"
+	conditionstring="can_jets0_met_beforeFit$events.jpg"
+	for z in $Jets0before
+	do
+		name="${z##*/}"
+		if [[ "$name" == "$conditionstring" ]];
+		then
+			output="0jets_$events.jpg"
+			convert +append $name $string $output
 		fi
 	done
 done
 
-for i in "${!jetsafter[@]}";
+Jetsbefore=$directory/can_jets_met_beforeFit*
+Jetsafter=$directory/can_jets_met_afterFit*
+for i in $Jetsafter
 do
-	
-	nameJeta="${jetsafter[$i]}"
-	label2a="$(cut -d "_" -f4 <<< $nameJeta)"
-	label3a="$(sed "s/[^0-9]//g" <<< $label2a)"
-	for z in "${!jetsafter[@]}";
-	do 
-		nameJetb="${jetsbefore[$z]}"
-		label2b="$(cut -d "_" -f4 <<< $nameJetb)"
-		label3b="$(sed "s/[^0-9]//g" <<< $label2b)"
-		if [[ "$label3b" == "$label3a" ]];
-		then 
-			inputa="can_jets_met_afterFit$label3a.jpg"
-			inputb="can_jets_met_beforeFit$label3b.jpg"
-			output="can_jets_met_$label3a.jpg"
-			convert +append $inputa $inputb $output
+	string="${i##*/}"
+	tempstring="$(cut -d "_" -f4 <<< $string)"
+	events="$(sed "s/[^0-9]//g" <<< $tempstring)"
+	conditionstring="can_jets_met_beforeFit$events.jpg"
+	for z in $Jetsbefore
+	do
+		name="${z##*/}"
+		if [[ "$name" == "$conditionstring" ]];
+		then
+			output="jets_$events.jpg"
+			finalout="Hist_$events.jpg"
+			final0j="0jets_$events.jpg"
+			convert +append $name $string $output
+			convert -append $output $final0j $finalout
+			rm $output
+			rm $final0j
 		fi
 	done
 done
