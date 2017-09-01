45281dad95ad6df945845ec82b141a7341869920
Thomas Nommensen <thomas.nommensen@coepp.org.au>
2017-09-01 20:17:36 +1000

    I have now adapted the number of events for each process from Isabel's data in the root file

--- ComandoGen.sh
+++ ComandoGen.sh
@@ -3,21 +3,18 @@
 #!/bin/ipython
 #============Generator text===================#
 initial=("1" "p p > t t~; p p > W- j; p p > W+ W-; p p > W- Z; p p > Z j; p p > Z Z;" "pptt; ppWj; ppWW; ppWZ; ppZj; ppZZ;" "y" "2" "p p > W+ j;" "4" "p p > W+ Z;" "7" "y")
+EventsInProcess=("" "608996" "1683" "85826" "24764" "26809" "13391")
 NumberOfProcess=6
-inside=("1" "2" "2" "6" "2")
+inside=("3" "1" "3500" "2" "3500" "3" "1" "2" "2" "6" "2")
 
 cd ~/Programs/HistFitter/
 source setup.sh
 cd ~/MadShell/
 
-i=0
-steps=100
-End=15000
-iterations=$(( (End - i)/steps ))
+iterations=1
 for ((x=1; x<= $iterations; x++));
 do
 	rm ~/Commands.txt
-	i=$((i+ $steps)) #<---- number of events
 	for command in "${initial[@]}"; #<----- writing the initial script for MadShell
 	do
 		echo "$command" >> Commands.txt
@@ -29,14 +26,15 @@ do
 		do
 			echo "$z" >> Commands.txt
 		done
-		echo "$i" >> Commands.txt
+		processevent="${EventsInProcess[$t]}"
+		echo "$processevent" >> Commands.txt   #EDIIITTTT HERE!!!!!!!!!!!!!!
 		echo "8" >> Commands.txt
 	done
 	echo "7" >> Commands.txt
 	bash MadShell.sh < Commands.txt
 	sleep 5
 	python DelphesReader.py 
-	HistFitter.py -w -f -D "before,after" /home/tnom6927/MadShell/Data/MyOneBinExample_6Tom.py >> logFile.txt
+	HistFitter.py -w -f -D "before,after" /home/tnom6927/MadShell/Data/MyOneBinExample_6Tom.py >> HistFitterLog.txt
 	mkdir Plots
 	cd ~/MadShell/results/Results
 	mv can_jets0_met_afterFit.pdf ~/MadShell/Plots/can_jets0_met_afterFit$i.pdf
