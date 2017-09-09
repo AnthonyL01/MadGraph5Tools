14f99af2227c818a0e8ed27dc060a7b8a162ca24
Thomas Nommensen <thomas.nommensen@coepp.org.au>
2017-09-10 03:43:17 +1000

    The current run

--- ComandoGen.sh
+++ ComandoGen.sh
@@ -3,18 +3,18 @@
 #!/bin/ipython
 #============Generator text===================#
 initial=("1" "p p > t t~; p p > W- j; p p > W+ W-; p p > W- Z; p p > Z j; p p > Z Z;" "pptt; ppWj; ppWW; ppWZ; ppZj; ppZZ;" "y" "2" "p p > W+ j;" "4" "p p > W+ Z;" "7" "y")
-EventsInProcess=("" "10000" "10000" "10000" "10000" "10000" "10000")
+EventsInProcess=("" "50000" "50000" "50000" "50000" "50000" "50000")
 NumberOfProcess=6
 inside=("3" "1" "6500" "2" "6500" "3" "1" "2" "2" "6" "2")
-
+ProcessNames=("pptt" "ppWj" "ppWW" "ppWZ" "ppZj" "ppZZ")
 cd ~/Programs/HistFitter/
 source setup.sh
 cd ~/MadShell/
 mkdir Plots
-iterations=1
+iterations=1000
 for ((x=1; x<= $iterations; x++));
 do
-	rm ~/Commands.txt
+	rm ~/MadShell/Commands.txt
 	for command in "${initial[@]}"; #<----- writing the initial script for MadShell
 	do
 		echo "$command" >> Commands.txt
@@ -28,19 +28,25 @@ do
 		done
 		processevent="${EventsInProcess[$t]}"
 		echo "$processevent" >> Commands.txt  
-		echo "8" >> Commands.txt
+		echo "6" >> Commands.txt
 	done
 	echo "7" >> Commands.txt
-	bash MadShell.sh < Commands.txt
+	#bash MadShell.sh < Commands.txt
 	sleep 5
-	python DelphesReader.py 
-	HistFitter.py -w -f -D "before,after" /home/tnom6927/MadShell/Data/MyOneBinExample_6Tom.py >> HistFitterLog.txt
-	cd ~/MadShell/results/Results
-	mv can_jets0_met_afterFit.pdf ~/MadShell/Plots/can_jets0_met_afterFit.pdf
-	mv can_jets0_met_beforeFit.pdf ~/MadShell/Plots/can_jets0_met_beforeFit.pdf
-	mv can_jets_met_afterFit.pdf ~/MadShell/Plots/can_jets_met_afterFit.pdf
-	mv can_jets_met_beforeFit.pdf ~/MadShell/Plots/can_jets_met_beforeFit.pdf
-	cd ~/MadShell
-	rm -rf results data config Commands.txt
+	#python DelphesReader.py 
+	#HistFitter.py -w -f -D "before,after" /home/tnom6927/MadShell/Data/MyOneBinExample_6Tom.py >> HistFitterLog.txt
+	#cd ~/MadShell/results/Results
+	#mv can_jets0_met_afterFit.pdf ~/MadShell/Plots/can_jets0_met_afterFit.pdf
+	#mv can_jets0_met_beforeFit.pdf ~/MadShell/Plots/can_jets0_met_beforeFit.pdf
+	#mv can_jets_met_afterFit.pdf ~/MadShell/Plots/can_jets_met_afterFit.pdf
+	#mv can_jets_met_beforeFit.pdf ~/MadShell/Plots/can_jets_met_beforeFit.pdf
+	#cd ~/MadShell
+	#rm -rf results data config Commands.txt
+	for copy in "${ProcessNames[@]}";
+	do 
+		DelphesROOT=~/Programs/MadGraph/bin/$copy/Events/run_01/tag_1_delphes_events.root
+		destination=~/MadShell/SimulationData/Delphes_Event$copy$x
+		cp $DelphesROOT $destination
+	done
 done
-python Extractor.py
+#python Extractor.py
