306c181e5b677076fcfc490f74e5074f4c52f8f9
Thomas Nommensen <thomas.nommensen@coepp.org.au>
2017-10-31 12:32:31 +1100

    Final version

--- ComandoGen.sh
+++ ComandoGen.sh
@@ -12,17 +12,19 @@ CommandsDir=$5
 SimulationName=$6
 Collection=$7
 Output=$8
-DelphesFCPTReader=$9
+DelphesFCPT=$9
 MadShellDir=${10}
 #======================================#
 E=50000
-initial=("$worker" "sm-ckm" "p p > t t~ QED=2 @4; p p > W- j QED=2 @4; p p > W+ W- QED=2 @4; p p > W- Z QED=2 @4; p p > Z j QED=2 @4; p p > Z Z QED=2 @4; p p > W- t QED=2 @4;" "pptt; ppWj; ppWW; ppWZ; ppZj; ppZZ; ppWt;" "y" "2" "p p > W+ j;" "4" "p p > W+ Z;" "8" "y")
-EventsInProcess=("" "$E" "$E" "$E" "$E" "$E" "$E" "$E")
-NumberOfProcess=7
-inside=("3" "1" "$Beam1" "2" "$Beam2" "3" "1" "2" "2" "4" "y" "6" "4" "y" "90900" "2")
-ProcessNames=("pptt" "ppWj" "ppWW" "ppWZ" "ppZj" "ppZZ" "ppWt")
+NumberOfProcess=8
+run=$((NumberOfProcess+1))
+initial=("$worker" "EFT" "p = g u u~ c c~ d d~ s s~ b b~" "p p > t t~ QED=2 @4; p p > W- j QED=2 @4; p p > W+ W- QED=2 @4; p p > W- Z QED=2 @4; p p > Z j QED=2 @4; p p > Z Z QED=2 @4; p p > W- t QED=2 @4; p p > ta+ mu- QED=2 @4;" "pptt; ppWj; ppWW; ppWZ; ppZj; ppZZ; ppWt; pptamu;" "y" "2" "p p > W+ j;" "4" "p p > W+ Z;" "$run" "y")
+EventsInProcess=("" "$E" "$E" "$E" "$E" "$E" "$E" "$E" "$E")
+inside=("3" "1" "$Beam1" "2" "$Beam2" "3" "1" "2" "2" "6" "4" "y" "90900" "2")
+ProcessNames=("pptt" "ppWj" "ppWW" "ppWZ" "ppZj" "ppZZ" "ppWt" "pptamu")
 for ((x=1; x<= $EndIteration; x++));
 do
+
 	ComandGen=$CommandsDir/Commands$worker.txt
 	sleep 5
 	rm $ComandGen > /dev/null 2>&1
@@ -41,17 +43,18 @@ do
 		echo "$processevent" >> $ComandGen 
 		echo "5" >> $ComandGen
 	done
-	echo "8" >> $ComandGen
+	echo "$run" >> $ComandGen
 	echo "w:$worker number:$x , dir:$ComandGen Time: $(date)"
 	bash $MadShellDir < $ComandGen 
-	
 	#====Copying the data to a different location====#
 	for copy in "${ProcessNames[@]}";
 	do 
 		mkdir $SimulationName/$worker > /dev/null 2>&1
 		echo "Copying $copy"
-		DelphesROOT=$CommandsDir/bin/$copy/Events/run_01_decayed_1/tag_1_delphes_events.root
+		DelphesROOT=$CommandsDir/bin/$copy/Events/run_01/tag_1_delphes_events.root
 		destination=$SimulationName/$worker/Delphes_Event$copy$worker$x.root
 		mv $DelphesROOT $destination
 	done
+	dir=$HOME/MadGraphShell/SimulationData/13000MeV/$worker/*
+	bash $Collection "$dir" "$DelphesFCPT" "$Output" 
 done
